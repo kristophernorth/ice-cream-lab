2bfdcc9a6e5c3dde2871cc2cb43ee7f135da87c0
kristophernorth <kristopher.north@gmail.com>
2024-10-30 08:50:06 -0600

    commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const iceCream = [
+  { name: 'Cookie Dough', price: 1.25, quantity: 0, type: 'scoop' },
+  { name: 'Vanilla', price: 1, quantity: 0, type: 'scoop' },
+  { name: 'Strawberry', price: 1.25, quantity: 0, type: 'scoop' },
+  { name: 'Sprinkles', price: 0.25, quantity: 0, type: 'topping' },
+  { name: 'Chocolate Chips', price: 0.25, quantity: 0, type: 'topping' },
+  { name: 'Cookie Chunks', price: 0.50, quantity: 0, type: 'topping' },
+  { name: 'Waffle Cone', price: 3.00, quantity: 0, type: 'vessel' },
+  { name: 'Sprinkle Bowl', price: 7.00, quantity: 0, type: 'vessel' },
+  { name: 'Cinnamon Roll Bowl', price 19.00, quantity: 0, type: 'vessel' },
+]
+
+function orderVanilla(vanilla) {
+  console.log('ordering vanilla')
+}
+
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+const itemsForSale = [
+  {
+    name: '10ft Skeleton',
+    price: 249.75,
+    quantity: 0
+  },
+  {
+    name: 'pumpkin',
+    price: 5.50,
+    quantity: 0
+  },
+  {
+    name: 'Vampire Teeth',
+    price: 2.99,
+    quantity: 0
+  },
+  {
+    name: 'Ghost Costume',
+    price: 13.99,
+    quantity: 0
+  },
+]
+console.log('itemsForSale')
